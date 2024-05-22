30dead5eeba49a03ae63c861476af6079fe7bda5
Muhammad Rehman <mrehman@broadpeak.pk>
2024-05-22 18:22:46 +0500

    initial commit

---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+// src/Counter.js
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './counterSlice';
+
+function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
